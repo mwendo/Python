e121407ed982ed4fd5957198c3622d801ba81fe9
Micah Wendorf <wendorfmicah0@gmail.com>
2021-04-05 14:02:11 -0500

    inital commit

--- fundamentals/Hello_World/hello_world.py
+++ fundamentals/Hello_World/hello_world.py
@@ -12,4 +12,6 @@ print("Hello" + name)	# with a +	-- this one should give us an error!
 fave_food1 = "pita bread"
 fave_food2 = "hummus"
 print("I love to eat {} and {}." .format(fave_food1, fave_food2)) # with .format()
-print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
+print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string yes there are
+
+
