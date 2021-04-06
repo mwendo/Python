007912f2d6c44c6a7830158ee136a4e74d05b87b
Micah Wendorf <wendorfmicah0@gmail.com>
2021-04-05 19:09:41 -0500

    initial commit

---
+++ fundamentals/Greetings.py
@@ -0,0 +1,33 @@
+print("---------- Part I ----------")
+your_name = input("What is your name?")
+print(f"Your name is: {your_name}")
+
+print("---------- Part II ----------")
+your_name = input("What is your name?")
+print(f"Your name is: {your_name}")
+
+if (your_name == "Micah"):
+    print("Hey, that's my name too!")
+
+print("---------- Part III ----------")
+names = []
+
+for x in range(0, 10, 1):
+    your_name = input("What is your name?")
+    names.append(your_name)
+print("It was nice to meet all of you!")
+
+print("---------- Part IV ----------")
+names = []
+
+for x in range(0, 10, 1):
+    your_name = input("What is your name?")
+    names.append(your_name)
+    if()
+print("It was nice to meet all of you!")
+
+
+
+
+# favorite_color = input('What is your favorite color? ') # input takes a prompt, which needs to be a string
+# print(f'Your favorite color is: {favorite_color}') #output, prints the color given to the console
---
+++ fundamentals/Rock_Paper_Scissors.py
@@ -0,0 +1,43 @@
+import random
+
+playerPoints = 0
+computerPoints = 0
+
+while playerPoints !=3 and computerPoints != 3:
+    your_choice = input("Enter a choice (rock,paper, scissors): ")
+    user_selection = ["rock", "paper", "scissors"]
+    cpu_selection = random.choice(user_selection)
+    
+    print(f"\nUser selection: {your_choice}, Opponent chose {cpu_selection}.\n")
+    if your_choice == cpu_selection:
+        print("A draw? How boring.. try again, this time with PASSION!")
+    elif your_choice == "rock":
+        if cpu_selection == "scissors":
+            print("Rock SMASH! You win!")
+            playerPoints += 1
+        else: 
+            print("Paper beats rock, Better luck next time!")
+            computerPoints += 1
+    elif your_choice == "paper":
+        if cpu_selection == "rock":
+            print("Paper destroys rock all day! You win!")
+            playerPoints += 1
+        else: 
+            print("Scissors slices right through your puny paper. \n Try again!")
+            computerPoints += 1
+    elif your_choice == "scissors":
+        if cpu_selection == "paper":
+            print("Scissors slices right through that paper. YOU WIN!")
+            playerPoints += 1
+        else: 
+            print("Rock SMASH! You lose!!!")
+            computerPoints += 1
+if playerPoints > computerPoints:
+    print("You win! game over.")
+else: 
+    playerPoints < computerPoints
+    print("You lose!")
+
+
+
+
