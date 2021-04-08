279c8a920e7ef52176060bfccf2e03fa0d217fd1
Micah Wendorf <wendorfmicah0@gmail.com>
2021-04-08 18:49:51 -0500

    initial commit

--- flask/hello_flask/server.py
+++ flask/hello_flask/server.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("users.html")
+
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
 
 @app.route("/users")
 def users():
