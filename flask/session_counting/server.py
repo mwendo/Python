3822770b6c11a16ea40186c59419d4ecbbcfd320
Micah Wendorf <wendorfmicah0@gmail.com>
2021-04-09 13:38:24 -0500

    initial commit

--- flask/session_counting/server.py
+++ flask/session_counting/server.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, redirect
+
 app = Flask(__name__)
 app.secret_key = "Mr. Nibbles wuz heer."
 
 @app.route("/")
 def counter():
-    if 'key_name' in session:
-        print('key exists!')
+    if 'counter' not in session:
+        session['counter'] = 1
     else:
-        print("key 'key_name' does NOT exist")
-return render_template("index.html")
+        session['counter'] += 1
+    return render_template("index.html")
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect("/")
 
 if __name__=="__main__":
     app.run(debug=True)
