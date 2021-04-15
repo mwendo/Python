ec830cd90a5121ab568cf8c1e0759ab7070818d5
Micah Wendorf <wendorfmicah0@gmail.com>
2021-04-15 13:47:34 -0500

    initial commit

--- flask_mysql/users/server.py
+++ flask_mysql/users/server.py
@@ -27,5 +27,47 @@ def create():
     mysql.query_db(query,data)
     return redirect('/')
 
+@app.route('/show_page/<int:people_id>')
+def show(people_id):
+    query = "SELECT * FROM people WHERE id = %(id)s;"
+    data = {
+        "id": people_id
+    }
+    people = connectToMySQL('people').query_db(query, data)
+    print(people)
+    return render_template("read(one).html", people = people[0])
+
+@app.route('/edit_page/<int:people_id>')
+def edit(people_id):
+    query = "SELECT * FROM people WHERE id = %(id)s;"
+    data = {
+        "id": people_id
+    }
+    people = connectToMySQL('people').query_db(query, data)
+    print(people)
+    return render_template("edit_page.html", people = people[0])
+
+@app.route('/update/<int:people_id>', methods=['POST'])
+def update(people_id):
+    query = "UPDATE people SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s, updated_at=NOW() WHERE id = %(id)s;"
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "id": people_id,
+    }
+    people = connectToMySQL('people').query_db(query, data)
+    print(people)
+    return redirect(f"/show_page/{people_id}")
+
+@app.route('/delete/<int:people_id>')
+def delete(people_id):
+    query = "DELETE FROM people WHERE id = %(id)s;"
+    data = {
+        "id": people_id
+    }
+    people = connectToMySQL('people').query_db(query, data)
+    return redirect("/")
+
 if __name__ == "__main__":
     app.run(debug=True)
